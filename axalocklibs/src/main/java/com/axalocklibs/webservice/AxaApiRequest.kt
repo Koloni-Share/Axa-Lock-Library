f04fb76993d51cb2054f8992f1203ce48881b5df
nirav <nirav@topsinfosolutions.com>
2022-08-26 14:22:01 +0530

    - Added Function for White Label Application. - Update Gradle file with latest one. - Made disconnect callback change as per user app requirement. - Axa Lock library to lock and unlock Axa lock. - Add code to reconnect axa lock if disconnect. - Optimize library.

--- axalocklibs/src/main/java/com/axalocklibs/webservice/AxaApiRequest.kt
+++ axalocklibs/src/main/java/com/axalocklibs/webservice/AxaApiRequest.kt
@@ -68,4 +68,68 @@ object AxaApiRequest {
                 }
             })
     }
+
+    fun callPOSTWhiteLabelAPI(
+        activity: Activity?,
+        url: String,
+        params: HashMap<String, Any>,
+        tag: String,
+        apiResponse: IAPIAxaResponse,
+        appVersion: String,
+        authorizationHeader: String,
+        bearerAuthorization: String,
+        isWhiteLable: Boolean,
+        whiteLabelPartnerKey: String,
+        whiteLablePartnerID: String
+    ) {
+
+        if (isWhiteLable) {
+            params.put(whiteLabelPartnerKey, whiteLablePartnerID)
+        }
+
+        Log.e("WS_PARAM_", "$tag  : $params")
+
+        val okHttpClient = OkHttpClient().newBuilder()
+            .connectTimeout(2, TimeUnit.MINUTES)
+            .readTimeout(2, TimeUnit.MINUTES)
+            .writeTimeout(2, TimeUnit.MINUTES)
+            .build()
+
+
+        Rx2AndroidNetworking.post(url)
+            .setOkHttpClient(okHttpClient)
+            .addBodyParameter("android_version", appVersion)
+            .addHeaders("Authorization", authorizationHeader)
+            .addBodyParameter(params)
+            .setPriority(Priority.MEDIUM)
+            .build()
+            .stringObservable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : Observer<String> {
+                override fun onSubscribe(d: Disposable) {}
+                override fun onNext(jsonObject: String) {
+                    response = jsonObject + ""
+                    Log.e("WS_RESP_", tag + " : " + response)
+                }
+
+                override fun onError(e: Throwable) {
+                    try {
+                        e.printStackTrace()
+                        apiResponse.onFailure(e.localizedMessage, tag)
+                    } catch (exception: Exception) {
+                        exception.printStackTrace()
+                    }
+                }
+
+                override fun onComplete() {
+                    try {
+                        apiResponse.onSuccess(response, tag)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        apiResponse.onFailure(e.localizedMessage, tag)
+                    }
+                }
+            })
+    }
 }
